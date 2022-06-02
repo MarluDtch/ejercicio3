5679804bab49513eee15267536130782559496e3
Marlu Dietrich <marlu.dietrich@gmail.com>
2022-06-01 23:26:00 -0300

    subiendo archivo

---
+++ js/style.js
@@ -0,0 +1,8 @@
+let nombre = prompt ("Bienvenido, antes que nada nos gustaria saber tu Nombre")
+
+let edad = prompt ("Ingrese su edad")
+if (edad<20) {
+    console.log ("Hola, como estas?")
+}else{
+    console.log ("Bienvenido. Buenos días!");
+}
